1c9a557dc94896eda27b84148b44a427467a66e2
jack yin <waring3yin@icloud.com>
2018-06-20 15:42:28 +0800

    token回复

--- server.php
+++ server.php
@@ -4,6 +4,10 @@ include __DIR__ . '/bootstrap.php';
 
 $app = Factory::officialAccount($config);
 
+$app->server->push(function ($message) {
+    return "wechatoken";
+});
+
 $response = $app->server->serve();
 
 // 将响应输出
