ece87dec561b141d0cbf819983d3de51347c18e6
jack yin <waring3yin@icloud.com>
2018-06-20 12:06:04 +0800

    init

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+include __DIR__ . '/vendor/autoload.php';
+
+echo 'hello';
+
+// $config = [
+//     'app_id' => 'wx3cf0f39249eb0exx',
+//     'secret' => 'f1c242f4f28f735d4687abb469072axx',
+
+//     // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
+//     'response_type' => 'array',
+
+//     'log' => [
+//         'level' => 'debug',
+//         'file' => __DIR__.'/wechat.log',
+//     ],
+// ];
+
+// $app = EasyWeChat\Factory::officialAccount($config);
