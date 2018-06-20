f07c356653bacccde162e4e6543801f796094985
jack yin <waring3yin@icloud.com>
2018-06-20 13:44:48 +0800

    授权callback

--- profile.php
+++ profile.php
@@ -4,7 +4,7 @@ include __DIR__ . '/bootstrap.php';
 
 $config['oauth'] = [
     'scopes'   => ['snsapi_userinfo'],
-    'callback' => '/oauth_callback.php'
+    'callback' => 'oauth_callback.php'
 ];
 
 $app = EasyWeChat\Factory::officialAccount($config);
