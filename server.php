2a7de7cad40cf232bbc8cd2f1cf38e827e7e9158
jack yin <waring3yin@icloud.com>
2018-06-20 15:55:23 +0800

    生产环境环境变量添加英雄会登录重导向地址

--- server.php
+++ server.php
@@ -1,8 +1,20 @@
 <?php
 
-include __DIR__ . '/bootstrap.php';
+include __DIR__ . '/vendor/autoload.php';
 
-$config['token'] = 'wechatoken';
+$config = [
+    'app_id' => 'wxff9c142e93044137',
+    'secret' => '28c654c04c3ab0b5e7866f6016d6c6f5',
+    'token' => 'wechatoken',
+
+    // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
+    'response_type' => 'array',
+
+    'log' => [
+        'level' => 'debug',
+        'file' => __DIR__.'/wechat.log',
+    ],
+];
 
 $app = Factory::officialAccount($config);
 
